b97bb9f5107ac4fe61be62017ce50ece2c4199b3
stas-shr <stasshr@gmail.com>
2023-04-24 17:34:50 +0300

    [GIP-2]-Сделал Таб-бар с тремя вкладками, установил версию IOS-13, добавил папки под MVVM

---
+++ Giphy/Controllers/GIPProfileVC.swift
@@ -0,0 +1,19 @@
+//
+//  GIPProfileVC.swift
+//  Giphy
+//
+//  Created by Стас on 24.04.2023.
+//
+
+import UIKit
+
+class GIPProfileVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+        title = "Profile"
+    }
+
+}
