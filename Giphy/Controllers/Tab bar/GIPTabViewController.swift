2ce1e54e559d455610c8782f258b2e303aa7a34b
stas-shr <stasshr@gmail.com>
2023-05-11 18:53:35 +0300

    Created custom tabbar

--- Giphy/Controllers/Tab bar/GIPTabViewController.swift
+++ Giphy/Controllers/Tab bar/GIPTabViewController.swift
@@ -6,16 +6,27 @@
 //
 
 import UIKit
+import SOTabBar
 
-final class GIPTabViewController: UITabBarController {
+final class GIPTabViewController: SOTabBarController {
     
     private var coordinators: [Coordinator] = []
+    
+    override func loadView() {
+        super.loadView()
+        SOTabBarSetting.tabBarTintColor = Asset.orange.color
+        SOTabBarSetting.tabBarCircleSize = CGSize(width: 60, height: 60)
+        SOTabBarSetting.tabBarHeight = 50.0
+        SOTabBarSetting.tabBarBackground = Asset.gray.color
+        SOTabBarSetting.tabBarSizeImage = 38
+        SOTabBarSetting.tabBarSizeSelectedImage = 30
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTabs()
     }
-    
+
     // MARK: - TabBars
     private func setupTabs() {
         
