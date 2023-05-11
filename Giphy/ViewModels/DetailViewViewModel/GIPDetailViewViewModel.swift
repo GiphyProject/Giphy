7a9f5eb9571b3f20686633063e44fffed8a64ebc
stas-shr <stasshr@gmail.com>
2023-05-11 13:03:11 +0300

    Fixed compiling error

---
+++ Giphy/ViewModels/DetailViewViewModel/GIPDetailViewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  GIPDetailViewViewModel.swift
+//  Giphy
+//
+//  Created by Адель Ахметшин on 03.05.2023.
+//
+
+import Foundation
+
+final class GIPDetailViewViewModel: NSObject {
+
+}
+//Multiple commands produce '/Users/stas/Library/Developer/Xcode/DerivedData/Giphy-dhzvzhlcsmjappfnwegbemevhhve/Build/Intermediates.noindex/Giphy.build/Debug-iphonesimulator/Giphy.build/Objects-normal/arm64/GIPDetailViewViewModel.stringsdata'
